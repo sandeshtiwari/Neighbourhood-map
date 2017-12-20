e166324d91d289837c58fb35b10b1d3efb8bdfd4
Sandesh Tiwari <sandeshtiwari@live.com>
2017-12-19 23:46:32 -0600

    added styles to display the map and wrote the initMap function to initialize the map

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 28.215384, lng: 83.945317},
+          zoom: 8
+        });
+      }
