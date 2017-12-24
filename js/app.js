3184e7ee09f06c858e33faed0c672de167f39d29
Sandesh Tiwari <sandeshtiwari@live.com>
2017-12-23 19:42:58 -0600

    added styles to the map

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ var locations = [
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
     	center: {lat: 40.7413549, lng: -73.9980244},
+        styles: getStyles(),
         zoom: 13
     });
     infoWindow = new google.maps.InfoWindow();
@@ -108,6 +109,75 @@ function initMap() {
           new google.maps.Point(10, 34),
           new google.maps.Size(21,34));
         return markerImage;
+      }
+
+      function getStyles(){
+        return styles = [
+          {
+            featureType: 'water',
+            stylers: [
+              { color: '#19a0d8' }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { color: '#ffffff' },
+              { weight: 6 }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { color: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -40 }
+            ]
+          },{
+            featureType: 'transit.station',
+            stylers: [
+              { weight: 9 },
+              { hue: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { lightness: 100 }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { lightness: -100 }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'on' },
+              { color: '#f0e4d3' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -25 }
+            ]
+          }
+        ];
       }
 
 
